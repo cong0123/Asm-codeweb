d58a7d4c9d16cdfe283babf1d7b7c451e6f05d3f
cong0123 <94831886+cong0123@users.noreply.github.com>
2022-12-28 16:07:19 +0700

    Add project files.

---
+++ Bok/Data/AppDbInitializer.cs
@@ -0,0 +1,265 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Bok.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bok.Data.Static;
+using Microsoft.AspNetCore.Identity;
+
+namespace Bok.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                context.Database.EnsureCreated();
+
+                //Cinema
+                if (!context.Publishers.Any())
+                {
+                    context.Publishers.AddRange(new List<Publisher>()
+                    {
+                        new Publisher()
+                        {
+                            Name = "Publisher 1",
+                            Logo = "http://dotnethow.net/images/cinemas/cinema-1.jpeg",
+                            Description = "This is the description of the first cinema"
+                        },
+                        new Publisher()
+                        {
+                            Name = "Publisher 2",
+                            Logo = "http://dotnethow.net/images/cinemas/cinema-2.jpeg",
+                            Description = "This is the description of the first cinema"
+                        },
+                        new Publisher()
+                        {
+                            Name = "Publisher 3",
+                            Logo = "http://dotnethow.net/images/cinemas/cinema-3.jpeg",
+                            Description = "This is the description of the first cinema"
+                        },
+                        new Publisher()
+                        {
+                            Name = "Publisher 4",
+                            Logo = "http://dotnethow.net/images/cinemas/cinema-4.jpeg",
+                            Description = "This is the description of the first cinema"
+                        },
+                        new Publisher()
+                        {
+                            Name = "Publisher 5",
+                            Logo = "http://dotnethow.net/images/cinemas/cinema-5.jpeg",
+                            Description = "This is the description of the first cinema"
+                        },
+                    });
+                    context.SaveChanges();
+                }
+                //Actors
+                if (!context.Authors.Any())
+                {
+                    context.Authors.AddRange(new List<Author>()
+                    {
+                        new Author()
+                        {
+                            FullName = "Actor 1",
+                            Bio = "This is the Bio of the first actor",
+                            ProfilePictureURL = "http://dotnethow.net/images/actors/actor-1.jpeg"
+
+                        },
+                        new Author()
+                        {
+                            FullName = "Actor 2",
+                            Bio = "This is the Bio of the second actor",
+                            ProfilePictureURL = "http://dotnethow.net/images/actors/actor-2.jpeg"
+                        },
+                        new Author()
+                        {
+                            FullName = "Actor 3",
+                            Bio = "This is the Bio of the second actor",
+                            ProfilePictureURL = "http://dotnethow.net/images/actors/actor-3.jpeg"
+                        },
+                        new Author()
+                        {
+                            FullName = "Actor 4",
+                            Bio = "This is the Bio of the second actor",
+                            ProfilePictureURL = "http://dotnethow.net/images/actors/actor-4.jpeg"
+                        },
+                        new Author()
+                        {
+                            FullName = "Actor 5",
+                            Bio = "This is the Bio of the second actor",
+                            ProfilePictureURL = "http://dotnethow.net/images/actors/actor-5.jpeg"
+                        }
+                    });
+                    context.SaveChanges();
+                }
+
+                //Movies
+                if (!context.Books.Any())
+                {
+                    context.Books.AddRange(new List<Book>()
+                    {
+                        new Book()
+                        {
+                            Name = "Life",
+                            Description = "This is the Life movie description",
+                            Price = 39.50,
+                            ImageURL = "http://dotnethow.net/images/movies/movie-3.jpeg",
+
+                           PublisherId = 1,
+                            BookCategory = BookCategory.Documentary
+                        },
+                        new Book()
+                        {
+                            Name = "The Shawshank Redemption",
+                            Description = "This is the Shawshank Redemption description",
+                            Price = 29.50,
+                            ImageURL = "http://dotnethow.net/images/movies/movie-1.jpeg",
+
+                            PublisherId = 2,
+                            BookCategory = BookCategory.Action
+                        },
+                        new Book()
+                        {
+                            Name = "Ghost",
+                            Description = "This is the Ghost movie description",
+                            Price = 39.50,
+                            ImageURL = "http://dotnethow.net/images/movies/movie-4.jpeg",
+
+                           PublisherId = 3,
+                            BookCategory = BookCategory.Horror
+                        },
+                        new Book()
+                        {
+                            Name = "Race",
+                            Description = "This is the Race movie description",
+                            Price = 39.50,
+                            ImageURL = "http://dotnethow.net/images/movies/movie-6.jpeg",
+
+                            PublisherId = 4,
+                            BookCategory = BookCategory.Documentary
+                        },
+                        new Book()
+                        {
+                            Name = "Scoob",
+                            Description = "This is the Scoob movie description",
+                            Price = 39.50,
+                            ImageURL = "http://dotnethow.net/images/movies/movie-7.jpeg",
+
+                            PublisherId = 5,
+                            BookCategory = BookCategory.Cartoon
+                        },
+                        new Book()
+                        {
+                            Name = "Cold Soles",
+                            Description = "This is the Cold Soles movie description",
+                            Price = 39.50,
+                            ImageURL = "http://dotnethow.net/images/movies/movie-8.jpeg",
+
+                            PublisherId = 2,
+                            BookCategory = BookCategory.Drama
+                        }
+                    });
+                    context.SaveChanges();
+                }
+                //Actors & Movies
+                if (!context.Authors_Books.Any())
+                {
+                    context.Authors_Books.AddRange(new List<Author_Book>()
+                    {
+                        new Author_Book()
+                        {
+                            AuthorId = 1,
+                            BookId = 1
+                        },
+                        new Author_Book()
+                        {
+                            AuthorId= 3,
+                            BookId = 1
+                        },
+
+                         new Author_Book()
+                        {
+                            AuthorId = 1,
+                            BookId = 2
+                        },
+                         new Author_Book()
+                        {
+                            AuthorId = 4,
+                            BookId = 2
+                        },
+
+                        new Author_Book()
+                        {
+                            AuthorId = 1,
+                            BookId = 3
+                        },
+
+
+                    });
+                    context.SaveChanges();
+                }
+            }
+
+      
+        }
+        public static async Task SeedUsersAndRolesAsync(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+
+                //Roles
+                var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                if (!await roleManager.RoleExistsAsync(UserRoles.Admin))
+                    await roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
+                if (!await roleManager.RoleExistsAsync(UserRoles.User))
+                    await roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+
+                //Users
+                var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+                string adminUserEmail = "admin@gmail.com";
+
+                var adminUser = await userManager.FindByEmailAsync(adminUserEmail);
+                if (adminUser == null)
+                {
+                    var newAdminUser = new ApplicationUser()
+                    {
+                        FullName = "Admin User",
+                        UserName = "admin-user",
+                        Email = adminUserEmail,
+                        EmailConfirmed = true
+                    };
+                    await userManager.CreateAsync(newAdminUser, "Admin@1");
+                    await userManager.AddToRoleAsync(newAdminUser, UserRoles.Admin);
+                }
+
+
+                string appUserEmail = "user@gmail.com";
+
+                var appUser = await userManager.FindByEmailAsync(appUserEmail);
+                if (appUser == null)
+                {
+                    var newAppUser = new ApplicationUser()
+                    {
+                        FullName = "Application User",
+                        UserName = "app-user",
+                        Email = appUserEmail,
+                        EmailConfirmed = true
+                    };
+                    await userManager.CreateAsync(newAppUser, "Admin@1");
+                    await userManager.AddToRoleAsync(newAppUser, UserRoles.User);
+                }
+            }
+        }
+    }
+}
+
+   
+
+       
+   
